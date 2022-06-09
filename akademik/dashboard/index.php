0784a95e2944fc994b024dd6b78143f6cf634cbd
deslow <80503952+deslow27@users.noreply.github.com>
2022-06-09 22:54:25 +0700

    membuat folder dashboard dan file index.php

---
+++ akademik/dashboard/index.php
@@ -0,0 +1,33 @@
+<?php include "../template/header.php" ?>
+
+<!-- Content Wrapper -->
+<div id="content-wrapper" class="d-flex flex-column">
+
+    <!-- Main Content -->
+    <div id="content">
+
+        <!-- Topbar -->
+        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+            <!-- Sidebar Toggle (Topbar) -->
+            <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                <i class="fa fa-bars"></i>
+            </button>
+
+
+            <!-- Begin Page Content -->
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                    <h1 class="h3 mt-4 mb-0 text-dark">Dashboard</h1>
+                </div>
+            </div>
+            <!-- End of Content Wrapper -->
+        </nav>
+    </div>
+
+</div>
+<!-- End of Page Wrapper -->
+
+<?php include "../template/footer.php" ?>
